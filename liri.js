07f8f2de79f9d4c7b48fb13a9e4bb9322f5ba998
Brandon Chang <Brandon Chang>
2017-04-05 23:49:26 -0700

    Added functionality so by default if no search is made (process.argv[3]), then it will automatically print out the 'the sign' as instructed. It should have the functionality to pick if there's multiple artists as well.

--- liri.js
+++ liri.js
@@ -15,12 +15,9 @@ var client = new twitterNPM({
 var spotifyNPM = require('spotify');
 var search = process.argv[3]
 
-
-
 	////// Inputs //////
 
-
-// When the user inputs my-tweets in their 
+// When the user inputs my-tweets, it will run this:
 if (process.argv[2] == "my-tweets") {
 	console.log("Test")
 var params = {screen_name: 'karunashi'};
@@ -38,20 +35,41 @@ client.get('statuses/user_timeline', params, function(error, tweets, response) {
   }
 });
 }
-
-if (process.argv[2] == "spotify-this-song") {
+// When the user inputs spotify-this-song, it will run this:
+if (process.argv[2] == "spotify-this-song" && !search == " ") {
 	spotifyNPM.search({ type: 'track', query: search }, function(err, data) {
 	    if ( err ) {
 	        console.log('Error occurred: ' + err);
 	        return;
 	    }
-	 	console.log("Artist(s) Name: "+data.tracks.items[0].album.artists[0].name);
+	    // For-loop to check and see if the artists array contains more than one object. If it does, it will print the first (if), if not, it will print the else statement. Used to check if there's singular or plural artists since there can be more than one artist.
+	    if (!search == " ") {
+
+	    for (var i = 0; i < data.tracks.items[0].artists.length; i++) {
+	    	if (data.tracks.items[0].artists.length > 1) {
+	    		console.log("Artist(s) Name: "+data.tracks.items[0].artists[i].name);
+	    	}
+	    	else {
+	    		console.log("There's only one artist.")
+	    		console.log("Artist Name: "+data.tracks.items[0].artists[i].name);
+	    	}
+	    }
 	 	console.log("Song Name: "+data.tracks.items[0].name);
 	 	console.log("Album Name: "+data.tracks.items[0].album.name);
 	 	console.log("Spotify Link: "+data.tracks.items[0].album.external_urls.spotify);
 	 	console.log("Preview Link: "+data.tracks.items[0].preview_url);
-	 	console.log(data.tracks.items[0]);
-
-	    // Do something with 'data' 
+	 	// console.log(data.tracks.items[0]); JUST FOR CHECKING [DELETE LATER]
+	}
 	});
 }
+else {
+spotifyNPM.search({ type: 'track', query: 'The Sign,  Ace of Base' }, function(err, data) {
+	    console.log("Artist Name: "+data.tracks.items[0].artists[0].name);
+	 	console.log("Song Name: "+data.tracks.items[0].name);
+	 	console.log("Album Name: "+data.tracks.items[0].album.name);
+	 	console.log("Spotify Link: "+data.tracks.items[0].album.external_urls.spotify);
+	 	console.log("Preview Link: "+data.tracks.items[0].preview_url);
+    // Do something with 'data' 
+});
+}
+
