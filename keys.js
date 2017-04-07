4066b6cd6b8c813dad5d79fe79b11682cc1372a4
Brandon Chang <Brandon Chang>
2017-04-06 17:13:05 -0700

    Regenerated key for my consumer key as well to ensure no compromise is made

--- keys.js
+++ keys.js
@@ -1,8 +1,8 @@
 console.log('this is loaded');
 
 exports.twitterKeys = {
-  consumer_key: 'vMgQhUsaKTiGv966MYDrOIgVW',
-  consumer_secret: 'RiPXK6sTgvY3TVlrWQHLiT2J3Yysv0YQQM8x3afVQcfipUlqcY',
+  consumer_key: '6d33WaS0gA4IlJlqJsCnx7yuW',
+  consumer_secret: 'MgPLad5tjqJixFgl8Yy5hh8YLvU7mGTg3FbcSSNBnaJ3TnL0dd',
   access_token_key: '106948916-17zEHqaxwGUkgYnNpnrMHypVVNAlbqCmdc5WtGFp',
   access_token_secret: 'SpesEWORioZ3R1IeX5Tr3ORX3gcSRmSI3aGezEDAysV9b',
 }
